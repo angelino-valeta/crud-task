848542b07b436a88a7ae76e80bc4494ce75521a6
Angelino Valeta <angelinovaleta@gmail.com>
2022-03-15 22:07:55 +0100

    cors

--- api/app.js
+++ api/app.js
@@ -4,4 +4,19 @@ const mongoose = require('./database/database');
 
 const app = express();
 
+// CORS
+app.use(function(req, res, next){
+
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, PATCH, DELETE');
+
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+	next();
+});
+
+// BODY PARSER
+app.use(express.json());
+
 app.listen(5000, () => { console.log('Server running on port 5000!')});
